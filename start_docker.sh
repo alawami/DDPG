31690c09b3b7b7255c73fbd4ac5a6ec8068dc555
alawami <ahmed.alawami+github@alumni.colostate.edu>
2020-06-20 20:51:04 -0600

    fix start docker issue

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --name reacher --rm -it -v "$(pwd)":/rl -p 8900:8888 aalawami/reacher_rl:deeprl --
+docker run --gpus all --name reacher --rm -it -v "$(pwd)":/rl -p 8900:8888 aalawami/reacher_rl:deeprl
