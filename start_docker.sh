a0816c7c076f5209b45d294946d146f82fb2ed81
alawami <ahmed.alawami+github@alumni.colostate.edu>
2020-08-10 10:34:19 -0600

    add docker flags

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --name reacher --rm -it -v "$(pwd)":/rl -p 8888:8888 aalawami/reacher_rl:cuda10.2 --
+docker run --gpus all --name reacher --rm -it -v "$(pwd)":/rl -p 8888:8888 --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 aalawami/banana_rl:cuda10.2 --
